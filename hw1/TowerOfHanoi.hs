e1fc4a0f512ef223e532b02c28eaef2550a26dd8
deathgrindfreak <john.cooper.bell@gmail.com>
2016-04-07 16:41:06 +0000

    Solved hanoi problem

--- hw1/TowerOfHanoi.hs
+++ hw1/TowerOfHanoi.hs
@@ -4,6 +4,10 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi p a b c = undefined
+hanoi d a b c =
+    if d == 0 then [(a, b)]
+    else hanoi (d - 1) a c b
+        ++ [(a, b)]
+        ++ hanoi (d - 1) c b a
 
 main = do print $ hanoi 2 "a" "b" "c"

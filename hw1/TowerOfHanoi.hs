65eac9a811a32698f40cdb23938279456a73c8db
Cooper Bell <john.cooper.bell@gmail.com>
2016-04-07 16:49:27 -0500

    Add hw2 files

--- hw1/TowerOfHanoi.hs
+++ hw1/TowerOfHanoi.hs
@@ -6,8 +6,6 @@ type Move = (Peg, Peg)
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi d a b c =
     if d == 0 then [(a, b)]
-    else hanoi (d - 1) a c b
-        ++ [(a, b)]
-        ++ hanoi (d - 1) c b a
+    else hanoi (d - 1) a c b ++ [(a, b)] ++ hanoi (d - 1) c b a
 
 main = do print $ hanoi 2 "a" "b" "c"

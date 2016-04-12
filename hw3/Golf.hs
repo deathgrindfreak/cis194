0c640df0d692704a9941b978de3717735f61d7b9
deathgrindfreak <john.cooper.bell@gmail.com>
2016-04-12 14:01:13 +0000

    Finish exercise 1 of hw3

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -1,7 +1,15 @@
 module Golf where
 
-skip :: [a] -> [[a]]
-skip x = undefined
+-- Exercise 1
 
-main :: IO ()
-main = undefined
+-- Takes an integer n and a list of elements and returns a list of every nth element
+skipN :: Int -> [a] -> [a]
+skipN n = map snd . filter (\(i, _) -> i `mod` n == 0) . zip [1..]
+
+-- Takes a list and returns a list of lists consisting of all the ith elements
+-- from i <- [1..(length list)]
+skip :: [a] -> [[a]]
+skip l = map ((flip skipN) l) [1..(length l)]
+
+
+-- Exercise 2

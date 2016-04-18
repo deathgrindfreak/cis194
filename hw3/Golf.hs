2324c9590efdf54b975e3b8cff3d81240ad0f146
Cooper Bell <john.cooper.bell@gmail.com>
2016-04-17 22:48:20 -0500

    Finish exercise 3

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -1,6 +1,7 @@
 module Golf where
 
 import Data.List
+import Data.Ord
 
 -- Exercise 1
 
@@ -11,7 +12,7 @@ skipN n = map snd . filter (\(i, _) -> i `mod` n == 0) . zip [1..]
 -- Takes a list and returns a list of lists consisting of all the ith elements
 -- from i <- [1..(length list)]
 skip :: [a] -> [[a]]
-skip l = map ((flip skipN) l) [1..(length l)]
+skip l = map (`skipN` l) [1..(length l)]
 
 
 -- Exercise 2
@@ -19,7 +20,7 @@ skip l = map ((flip skipN) l) [1..(length l)]
 localMaxima :: [Int] -> [Int]
 localMaxima [] = []
 localMaxima [x] = []
-localMaxima (x:y:[]) = []
+localMaxima [x,y] = []
 localMaxima (x:y:z:xs)
     | x < y && z < y = y : localMaxima (y:z:xs)
     | otherwise = localMaxima (y:z:xs)
@@ -28,18 +29,17 @@ localMaxima (x:y:z:xs)
 -- Exercise 3
 
 hist :: [Int] -> [(Int, Int)]
-hist = map (\l@(x:_) -> (x, length l)) . groupBy (==) . sort
+hist = map (\l@(x:_) -> (x, length l)) . group . sort
 
-rpt :: Int -> Char -> String
-rpt x ch = take x $ repeat ch
+addZ :: [(Int, Int)] -> [(Int, Int)]
+addZ l = let m = map (\x -> (x, 0)) $ filter (`notElem` map fst l) [0..9]
+         in m ++ l
 
 histStr :: [(Int, Int)] -> String
 histStr l = let m = (maximum . map snd) l 
-            in map (\(n, x) -> (rpt (m - x) ' ') ++ (rpt x '*') ++ "=" ++ show n) l
+                f =  map (\(n, x) -> replicate (m - x) ' ' ++ replicate x '*' 
+                                        ++ "=" ++ show n) l
+                in intercalate "\n" $ transpose f
 
-{-
-histStr l = let m = maximum . map snd l
-                wh w = map (\_ -> ' ') [1..w] in 
-                undefined
-    --intercalate "\n" $ transpose $ 
--}
+histogram :: [Int] -> String
+histogram = histStr . sortBy (comparing fst) . addZ . hist

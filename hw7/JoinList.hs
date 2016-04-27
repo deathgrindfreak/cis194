513b68b343532ce881d0dbaa9512df5424f9e5c4
deathgrindfreak <john.cooper.bell@gmail.com>
2016-04-27 22:02:15 +0000

    Add to hw7

--- hw7/JoinList.hs
+++ hw7/JoinList.hs
@@ -1,6 +1,19 @@
 module JoinList where
 
+import Data.Monoid
 import Sized
+
+(!!?) :: [a] -> Int -> Maybe a
+[]     !!? _         = Nothing
+_      !!? i | i < 0 = Nothing
+(x:xs) !!? 0         = Just x
+(x:xs) !!? i         = xs !!? (i - 1)
+
+jlToList :: JoinList m a -> [a]
+jlToList Empty            = []
+jlToList (Single _ a)     = [a]
+jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
+
 
 -- Exercise 1
 
@@ -10,7 +23,7 @@ data JoinList m a = Empty
     deriving (Show, Eq)
 
 tag :: Monoid m => JoinList m a -> m
-tag (Single m a) = m
+tag (Single m _) = m
 tag (Append m _ _) = m
 
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a  
@@ -20,8 +33,13 @@ tag (Append m _ _) = m
 
 -- Exercise 2
 
+jSize :: Sized b => JoinList b a -> Int
+jSize = getSize . tag
+
 indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
 indexJ _ Empty        = Nothing
 indexJ 0 (Single _ a) = Just a
 indexJ i (Single _ _) = Nothing
-indexJ 0 (Append _ _ _) = Nothing
+indexJ i (Append _ l r)
+    | jSize l > i = indexJ (i - 1) l
+    | otherwise  = indexJ (i - 1) r 

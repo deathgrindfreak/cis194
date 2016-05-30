99fb6dfb4a568c44b8153361d7ef947d5e739f88
Cooper Bell <john.cooper.bell@gmail.com>
2016-05-30 01:36:24 -0500

    Add optional exercises

---
+++ hw11/ApplicativeExamples.hs
@@ -0,0 +1,15 @@
+module ApplicativeExamples where
+
+import Control.Applicative
+
+(*>) :: Applicative f => f a -> f b -> f b
+_ *> b = b
+
+mapA' :: Applicative f => (a -> f b) -> ([a] -> f [b])
+mapA' f = sequenceA' . map f
+
+sequenceA' :: Applicative f => [f a] -> f [a]
+sequenceA' = foldr (\x acc -> (:) <$> x <*> acc) (pure [])
+
+replicateA :: Applicative f => Int -> f a -> f [a]
+replicateA n a = sequenceA' $ replicate n a
